88676fb694c67457863072c4fb91570c811636ca
stronray <stronray@gmail.com>
2014-05-24 09:38:02 +0000

    removing the payment step in checkout. Looks like only one decorater is executed

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,7 +2,9 @@ Spree::Order.class_eval do
   belongs_to :store
   scope :by_store, lambda { |store| where(:store_id => store.id) }
 
-  def available_payment_methods
-    @available_payment_methods ||= Spree::PaymentMethod.available(:front_end, store)
-  end
+  remove_checkout_step :payment
+
+#  def available_payment_methods
+#    @available_payment_methods ||= Spree::PaymentMethod.available(:front_end, store)
+#  end
 end
